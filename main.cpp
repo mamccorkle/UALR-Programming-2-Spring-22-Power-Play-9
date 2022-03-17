0f80c5cb82bf7046fbbb223710ea175498263968
mamccorkle <89654757+mamccorkle@users.noreply.github.com>
2022-03-17 18:07:59 -0500

    202203171806 - Initial Commit
    Added VCS-Git
    
    Added Remotes - GitHub, Gitea
    
    Created document header

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//
+//  Project: UALR - Programming 2 - Spring 22 - Power Play 9
+//  Created by: Mark McCorkle on 20220317
+//  Based on: Code Provided by Sean Orme
+//  IDE: CLion 2021.2.3     - UNVERIFIED
+//  IDE: XCode              - UNVERIFIED
+//  IDE: Visual Studio 2022 - UNVERIFIED
+//  IDE: Linux via g++      - UNVERIFIED (g++ -std=c++17 -Wall main.cpp Item.cpp Player.cpp Monster.cpp Object.cpp -o main)
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    //https://github.com/mamccorkle/UALR-Programming-2-Spring-22-Power-Play-9.git
+    //http://localhost:3000/decyple/UALR-Programming-2-Spring-22-Power-Play-9.git
+
+    return 0;
+}
